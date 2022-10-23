99d429e18c9979d914488f679befc7bbf9649018
Christopher Bellanger <christopher.bellanger@teester.com>
2022-10-23 20:12:57 +0200

    remove static props

--- src/pages/posts/[slug].jsx
+++ src/pages/posts/[slug].jsx
@@ -24,38 +24,38 @@ export default function PostPage({ source, frontMatter }) {
   );
 }
 
-export const getStaticProps = async ({ params }) => {
-  const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`);
-  const source = fs.readFileSync(postFilePath);
+// export const getStaticProps = async ({ params }) => {
+//   const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`);
+//   const source = fs.readFileSync(postFilePath);
 
-  const { content, data } = matter(source);
+//   const { content, data } = matter(source);
 
-  const mdxSource = await serialize(content, {
-    // Optionally pass remark/rehype plugins
-    mdxOptions: {
-      remarkPlugins: [],
-      rehypePlugins: [],
-    },
-    scope: data,
-  });
+//   const mdxSource = await serialize(content, {
+//     // Optionally pass remark/rehype plugins
+//     mdxOptions: {
+//       remarkPlugins: [],
+//       rehypePlugins: [],
+//     },
+//     scope: data,
+//   });
 
-  return {
-    props: {
-      source: mdxSource,
-      frontMatter: data,
-    },
-  };
-};
+//   return {
+//     props: {
+//       source: mdxSource,
+//       frontMatter: data,
+//     },
+//   };
+// };
 
-export const getStaticPaths = async () => {
-  const paths = postFilePaths
-    // Remove file extensions for page paths
-    .map((path) => path.replace(/\.mdx?$/, ''))
-    // Map the path into the static paths object required by Next.js
-    .map((slug) => ({ params: { slug } }));
+// export const getStaticPaths = async () => {
+//   const paths = postFilePaths
+//     // Remove file extensions for page paths
+//     .map((path) => path.replace(/\.mdx?$/, ''))
+//     // Map the path into the static paths object required by Next.js
+//     .map((slug) => ({ params: { slug } }));
 
-  return {
-    paths,
-    fallback: false,
-  };
-};
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
